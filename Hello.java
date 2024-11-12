fb04353f25a886348a0aa3eeeda49ab7cdea459e
RCDEV-RX <rodrigobentokostah@gmail.com>
2024-11-12 12:14:02 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//package not_a_project;
+
+import java.util.*;
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println(hello());
+    }
+    public static Integer hello(){
+        System.out.println("hello world".toUpperCase());
+        
+        return 19;
+    }
+}
+
